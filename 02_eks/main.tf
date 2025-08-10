1818810b1cd9852172aa1357393c67c1dd1dcdfa
sainyusuf <k4mbingoce@yahoo.com>
2025-08-10 23:44:32 +0200

    removed efs driver

--- 02_eks/main.tf
+++ 02_eks/main.tf
@@ -24,10 +24,10 @@ module "eks" {
       most_recent              = true
       service_account_role_arn = module.ebs_csi_irsa.iam_role_arn
     }
-    aws-efs-csi-driver = {
-      most_recent              = true
-      service_account_role_arn = module.efs_csi_irsa.iam_role_arn
-    }
+    # aws-efs-csi-driver = {
+    #   most_recent              = true
+    #   service_account_role_arn = module.efs_csi_irsa.iam_role_arn
+    # }
   }
 
 
@@ -127,21 +127,21 @@ module "ebs_csi_irsa" {
   tags = var.tags
 }
 
-module "efs_csi_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.58.0"
+# module "efs_csi_irsa" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "5.58.0"
 
-  role_name             = "${local.prefix}-efs-csi"
-  attach_efs_csi_policy = true
+#   role_name             = "${local.prefix}-efs-csi"
+#   attach_efs_csi_policy = true
 
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:efs-csi-controller-sa"]
-    }
-  }
-  tags = var.tags
-}
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:efs-csi-controller-sa"]
+#     }
+#   }
+#   tags = var.tags
+# }
 
 module "alb_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
